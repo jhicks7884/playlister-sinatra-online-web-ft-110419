0b870504e2bcb0e5a29485d0a80ce6b206b7629f
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-15 14:31:59 +0000

    Automatically backed up by Learn

--- db/migrate/20200115143131_create_artists_genres_songs.rb
+++ db/migrate/20200115143131_create_artists_genres_songs.rb
@@ -1,4 +1,18 @@
 class CreateArtistsGenresSongs < ActiveRecord::Migration[5.2]
   def change
-  end
-end
+     create_table :artists do |a|
+       a.string :name
+       a.timestamps null: false
+     end
+ 
+      create_table :genres do |g|
+       g.string :name
+       g.timestamps null: false
+     end
+ 
+      create_table :songs do |s|
+       s.string :name
+       s.integer :artist_id
+     end
+   end
+ end
